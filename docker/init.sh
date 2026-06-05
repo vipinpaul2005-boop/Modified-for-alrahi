8978a35973b2c02eb2c37dd2fdd24318238a14e3
vipinpaul2005-boop <vipinpaul2005@gmail.com>
2026-06-05 09:33:45 +0530

    Update init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -30,19 +30,19 @@ sed -i '/watch/d' ./Procfile
 bench get-app erpnext
 bench get-app hrms
 
-bench new-site hr.terreconsultancy.com \
+bench new-site hr.alrahigroup.com \
   --force \
   --mariadb-root-password 123 \
   --admin-password admin \
   --no-mariadb-socket
 
-bench --site hr.terreconsultancy.com install-app erpnext
-bench --site hr.terreconsultancy.com install-app hrms
+bench --site hr.alrahigroup.com install-app erpnext
+bench --site hr.alrahigroup.com.com install-app hrms
 
-bench --site hr.terreconsultancy.com set-config developer_mode 1
-bench --site hr.terreconsultancy.com enable-scheduler
-bench --site hr.terreconsultancy.com clear-cache
+bench --site hr.alrahigroup.com set-config developer_mode 1
+bench --site hr.alrahigroup.com enable-scheduler
+bench --site hr.alrahigroup.com clear-cache
 
-bench use hr.terreconsultancy.com
+bench use hr.alrahigroup.com
 
 bench start
